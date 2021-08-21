b5f4a04e0f3efd9c70b5a380dfd1150706689dda
MonikaKeller <70961788+MonikaKeller@users.noreply.github.com>
2021-08-21 19:01:46 +0200

    Add files via upload

--- src/test/java/ImdbRepeatedDataEntryTest.java
+++ src/test/java/ImdbRepeatedDataEntryTest.java
@@ -1,4 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImdbRepeatedDataEntryTest extends ImdbTest{
+
+    public static final String WATCHLIST_UPLOAD_FILE = "src/test/resources/teszt.txt";
+
 
     @Override
     public Imdb createPage(){
@@ -10,5 +24,30 @@ public class ImdbRepeatedDataEntryTest extends ImdbTest{
 
         //castolás
         return (ImdbRepeatedDataEntry) super.getPage();
+    }
+
+    @Test
+    public void repeatedDataEntryTest(){
+        getPage().signIn();
+        List<String> titles = openFile();
+        getPage().repeatedDataEntry(titles);
+        //Assertions.assertEquals();
+    }
+
+    public List<String> openFile(){
+        List<String> titles = new ArrayList<>();
+        try {
+            File file = new File(WATCHLIST_UPLOAD_FILE);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                 titles.add(line);
+            }
+            reader.close();
+            return titles;
+
+        } catch (IOException e) {
+            Assertions.fail("The file not exist", e);
+        }
     }
 }
